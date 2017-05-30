a362cda115a478f305542a6c2ee33c00302d05e2
Balazs Kovacs <Balazs Kovacs>
2017-05-30 11:48:56 +0300

    javadoc update

--- src/main/java/hu/inf/unideb/prt/bazs/towerdefence/controller/TileBuilder.java
+++ src/main/java/hu/inf/unideb/prt/bazs/towerdefence/controller/TileBuilder.java
@@ -1,13 +1,38 @@
 package hu.inf.unideb.prt.bazs.towerdefence.controller;
 
+/*-
+ * #%L
+ * towerdefence
+ * %%
+ * Copyright (C) 2017 University of Debrecen
+ * %%
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+ * #L%
+ */
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Created by Balazs on 2017-05-21.
  * This class is similar to the {@link RoadBuilder} class, it initializes a 10X10 grid that covers the field.
  * This grid shows which tiles are available for the towers, the builder class is needed so the path of the
- * road can be followed easily thus making it off limits for the towers its easy
+ * road can be followed easily thus making it off limits for the towers its easy.
  */
 public class TileBuilder {
     /**
